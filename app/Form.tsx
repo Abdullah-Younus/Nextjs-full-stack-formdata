bc6d7061b44995fdf0d62f86b501e6cbdafb3c1b
Abdullah-Youns <126175476+Abdullah-Youns@users.noreply.github.com>
2023-05-03 11:23:39 -0700

    added

---
+++ app/Form.tsx
@@ -0,0 +1,59 @@
+"use client"
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+function Form() {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const formHandler = async (data: any) => {
+        console.log(data);
+        await fetch("/api/formHandler", {
+            method: "POST",
+            body: JSON.stringify(data)
+        });
+
+    }
+
+
+    return (
+        <>
+            <div className='mx-10 my-10'>
+                <h1>React Hook Form</h1>
+                <form className='flex space-x-10' onSubmit={handleSubmit(formHandler)}>
+                    <div>
+                        <label>Email:</label>
+                        <input type="email"
+                            placeholder='Enter Your Email'
+                            {...register("email", {
+                                required: true
+                            })}
+                            className='border' />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <input
+                            type="password"
+                            placeholder='Enter Your Password'
+                            className='border'
+                            {...register("password", {
+                                required: true
+                            })}
+                        />
+                    </div>
+                    <div>
+                        <button type='submit'>Login</button>
+                    </div>
+                </form>
+            </div>
+
+
+        </>
+    )
+}
+
+export default Form;
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import Form from './Form';
+
+export default function Home() {
+  return (
+    <>
+      <Form />
+    </>
+  )
+}
